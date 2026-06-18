82bba7f84ed98c7d36f4c22068fac6aa750bac90
1F10250238 <s1f102502384@iniad.org>
2026-06-18 11:41:53 +0900

    トップページを追加

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.utils.timezone import make_aware
+from django.utils.dateparse import parse_datetime
+from todo.models import Task
 
 # Create your views here.
+def index(request):
+    if request.method == 'POST':
+        due_at = request.POST.get('due_at')
+        task = Task(
+            title=request.POST['title'],
+            due_at=make_aware(parse_datetime(due_at)) if due_at else None,
+        )
+        task.save()
+
+    tasks = Task.objects.all()
+
+    context = {
+        'tasks': tasks,
+    }
+    return render(request, 'todo/index.html', context)
